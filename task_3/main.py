b09019e561de9f5523f17dab80a84699dacfc1c9
elessarelfstone <elessarelfstone@mail.ru>
2023-10-19 23:42:52 +0500

    task 3 done

--- task_3/main.py
+++ task_3/main.py
@@ -1,1 +1,23 @@
-import
+import sys
+
+import redis
+
+
+def make_dict_from_arr(arr, ):
+    keys = arr[::2]
+    values = arr[1::2]
+    return dict(zip(keys, values))
+
+
+args = sys.argv[1:]
+client = redis.Redis()
+ns_key = args.pop(0)
+
+ns = client.incrby(ns_key)
+
+# kvs = make_dict_from_arr(args)
+
+for i, k in enumerate(args):
+    client.set(f'{ns_key}-{ns}-{i}', k)
+
+
